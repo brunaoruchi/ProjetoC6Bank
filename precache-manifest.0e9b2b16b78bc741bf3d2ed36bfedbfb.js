0acea85b37b61f06326db72e83d4ac3e8dde453c
Bruna Oruchi <brunaoruchi@alunos.utfpr.edu.br>
2020-09-11 18:03:14 -0300

    Updates

---
+++ precache-manifest.0e9b2b16b78bc741bf3d2ed36bfedbfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f17fc81e4176f3bb44384e09f8c296",
+    "url": "/ProjetoC6Bank/index.html"
+  },
+  {
+    "revision": "f3be202bd0b56efc9b59",
+    "url": "/ProjetoC6Bank/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "7efee205ee05db2d5c32",
+    "url": "/ProjetoC6Bank/static/js/2.068f90a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ProjetoC6Bank/static/js/2.068f90a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3be202bd0b56efc9b59",
+    "url": "/ProjetoC6Bank/static/js/main.62339c34.chunk.js"
+  },
+  {
+    "revision": "470f4aff6c9e89f68a99",
+    "url": "/ProjetoC6Bank/static/js/runtime-main.8b41d248.js"
+  }
+]);
